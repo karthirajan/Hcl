21cc20ec6754ca7a96096c04209526e77dfd0d8b
RenugaR <pavi.renu95@gmail.com>
2019-05-31 15:18:32 +0530

    Renu-changed the code

--- src/org/hcl/Java.java
+++ src/org/hcl/Java.java
@@ -1,16 +1,22 @@
 package org.hcl;
 
 public class Java {
-	public void test3() {
-		System.out.println("test3");
-		
-
+	public void companyName(String name) {
+		System.out.println("Company Name is\t"+name);
 	}
-	public void test4() {
-		System.out.println("test4");
-		System.out.println("i did");
-		
-
+	public void companyName(int Id,float Sal) {
+		System.out.println("ID\t"+Id+"\tSal\t"+Sal);
 	}
-
+	public void companyName(float NetSal,int Serialno,String location) {
+		System.out.println("NetSal\t"+NetSal+"\tSerial no\t"+Serialno+"\tLocation\t"+location);
+	}
+	
+	public static void main(String[] args) {
+		Java c=new Java();
+		c.companyName("ACL");
+		c.companyName(5465, 4612312f);
+		c.companyName(5465, 5, "Chennai");
+	}
+			
 }
+
--- src/org/hcl/Welcome.java
+++ src/org/hcl/Welcome.java
@@ -1,16 +1,17 @@
 package org.hcl;
 
-public class Welcome {
+public class Welcome extends Java {
 	public void test1() {
 		System.out.println("test1");
-		
-
 	}
 	public void test3() {
 		System.out.println("test2");
 		System.out.println("i made some changes");
-		
-
 	}
+	public static void main(String[] args) {
+	
 
+		Welcome w=new Welcome();
+		w.companyName("Renu");
+	}
 }
