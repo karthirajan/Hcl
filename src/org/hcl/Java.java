f28747421793860f6d058949736125def6c71e7e
unknown <sivapriya3793@gmail.com>
2019-05-31 11:39:44 +0530

    changes by priya

--- src/org/hcl/Java.java
+++ src/org/hcl/Java.java
@@ -1,6 +1,17 @@
 package org.hcl;
 
 public class Java {
+	public void test1() {
+		System.out.println("test1");
+	
+		
+
+	}
+	public void test2() {
+		System.out.println("test2");
+		
+
+	}
 	public void test3() {
 		System.out.println("test3");
 		
