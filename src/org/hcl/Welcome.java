7840c1e7d7308053c4b72cb6014f941eb36bcf15
manishnaga <mailtomaniya92@gmail.com>
2019-05-31 21:53:50 +0530

    commiting changes here

--- src/org/hcl/Welcome.java
+++ src/org/hcl/Welcome.java
@@ -1,6 +1,5 @@
 package org.hcl;
 
-<<<<<<< HEAD
 public class Welcome extends Java {
 	public void test1() {
 		System.out.println("test1");
@@ -8,21 +7,14 @@ public class Welcome extends Java {
 	public void test3() {
 		System.out.println("test2");
 		System.out.println("i made some changes");
-=======
-public class Welcome {
-	public static void main(String[] args) {
-		String s="Welcome to Greens Technology";
-		String s2[]=s.split(" ");
-		for(int i=s2.length-1; i>=0; i--) {
-			System.out.println(s2[i]);
-			
-		}
->>>>>>> 60560c8a467a49fdeb695bf2414bdfddb35a6849
+	}
+	public void empID() {
+		System.out.println("employee method");
+
 	}
 	public static void main(String[] args) {
-	
-
 		Welcome w=new Welcome();
+		w.empID();
 		w.companyName("Renu");
 	}
 }
