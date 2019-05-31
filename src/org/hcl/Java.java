268ea923b111b00b3737a00c6db06a6451c58755
manishnaga <mailtomaniya92@gmail.com>
2019-05-31 17:30:00 +0530

    commit changes by done

--- src/org/hcl/Java.java
+++ src/org/hcl/Java.java
@@ -1,31 +1,26 @@
 package org.hcl;
 
 public class Java {
-<<<<<<< HEAD
 	public void companyName(String name) {
 		System.out.println("Company Name is\t"+name);
 	}
 	public void companyName(int Id,float Sal) {
 		System.out.println("ID\t"+Id+"\tSal\t"+Sal);
-=======
+	}
 	public void test1() {
 		System.out.println("test1");
-	
-		
-
 	}
 	public void test2() {
 		System.out.println("test2");
-	
-
 	}
 	public void test4() {
 		System.out.println("test4");
-		System.out.println("i did");
-		
-
->>>>>>> 60560c8a467a49fdeb695bf2414bdfddb35a6849
+		System.out.println("i did");	
 	}
+	public void compID(int id) {
+		System.out.println("company id is" + id);
+	}
+	
 	public void companyName(float NetSal,int Serialno,String location) {
 		System.out.println("NetSal\t"+NetSal+"\tSerial no\t"+Serialno+"\tLocation\t"+location);
 	}
@@ -35,6 +30,7 @@ public class Java {
 		c.companyName("ACL");
 		c.companyName(5465, 4612312f);
 		c.companyName(5465, 5, "Chennai");
+		c.compID(7);
 	}
 			
 }
