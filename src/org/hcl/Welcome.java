1f4c559d8a1efacaf4b70a20a24c886ab3fa46d8
manishnaga <mailtomaniya92@gmail.com>
2019-05-31 22:03:45 +0530

    commit changes success

--- src/org/hcl/Welcome.java
+++ src/org/hcl/Welcome.java
@@ -9,21 +9,14 @@ public class Welcome extends Java {
 		System.out.println("test2");
 		System.out.println("i made some changes");
 	}
-public class Welcome {
-	public static void main(String[] args) {
-		String s="Welcome to Greens Technology";
-		String s2[]=s.split(" ");
-		for(int i=s2.length-1; i>=0; i--) {
-			System.out.println(s2[i]);
-			
-		}
+	public void empID() {
+		System.out.println("employee method");
 
 	}
 	public static void main(String[] args) {
-
 		Welcome w=new Welcome();
-		w.companyName("ASAR");
-		w.test4("i did ");
+		w.empID();
+		w.companyName("Renu");
 	}
-}
+	}
 }
