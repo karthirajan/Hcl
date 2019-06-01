e124273a83e4c097d9959b23aead3a48218682e7
lavanyacs94 <prathapzenith@gmail.com>
2019-06-01 17:25:36 +0530

    this is new mc

---
+++ src/org/hcl/Java6.java
@@ -0,0 +1,15 @@
+package org.hcl;
+
+public class Java6 {
+	
+	private void jav6() {
+
+		System.out.println("j6");
+	}
+	public static void main(String[] args) {
+		
+		Java6 j= new Java6();
+		j.jav6();
+	}
+
+}
